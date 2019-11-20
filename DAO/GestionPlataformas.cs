dc0dd385c6e5cf886976b6888f8575fa39d00a62
Your Name <you@example.com>
2019-11-20 04:28:21 -0300

    HJG

--- DAO/GestionPlataformas.cs
+++ DAO/GestionPlataformas.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace DAO
 {
     class GestionPlataformas
     {
+        public GestionPlataformas()
+        {
+
+        }
+        AccesoDatos obj = new AccesoDatos();
+        public DataTable ObtenerTodasLasPlataformas()
+        {
+            return obj.ObtenerTabla("Plataformas", "Select * from Plataformas");
+        }
 
     }
 }
