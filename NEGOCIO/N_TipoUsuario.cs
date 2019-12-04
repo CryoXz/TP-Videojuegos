389c37cf2e8b1327223b92d19b79e669f2fa2e09
s-robledo <seru1909@live.com.ar>
2019-12-04 00:05:32 -0300

    clase N_usuario clase N_TipoUsuario

---
+++ NEGOCIO/N_TipoUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using ENTIDAD;
+
+namespace NEGOCIO
+{
+    public class N_TipoUsuario
+    {
+        public DataTable getTabla()
+        {
+            DaoTipoUsuario dao = new DaoTipoUsuario();
+            return dao.getTablaTipoUsuario();
+        }
+        public DataSet getTipoUsuario()
+        {
+
+            DaoTipoUsuario dao = new DaoTipoUsuario();
+            DataSet data = dao.getConsultaTipoUsuario();
+            return data;
+        }
+    }
+}
---
+++ NEGOCIO/N_Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using ENTIDAD;
+
+namespace NEGOCIO
+{
+    public class N_Usuario
+    {
+        public void GuardarUsuario(String tipoUsuario, String nombre, String apellido, String nickname, String contraseña, String dni, DateTime fechaNacimiento, String email, String telefono, String direccion)
+        {
+            TipoUsuario o_tipoUsuario = new TipoUsuario();
+            o_tipoUsuario.setCodigoTipoUsuario(tipoUsuario);
+            DaoUsuario daoUsuario = new DaoUsuario();
+            Usuario usuarioNuevo = new Usuario();
+            usuarioNuevo.setCodigoUsuario("u4");
+            usuarioNuevo.setIdTipoUsuario(o_tipoUsuario);
+            usuarioNuevo.setNombre(nombre);
+            usuarioNuevo.setApellido(apellido);
+            usuarioNuevo.setNickname(nickname);
+            usuarioNuevo.SetContraseña(contraseña);
+            usuarioNuevo.setDni(dni);
+            usuarioNuevo.setFechaNacimiento(fechaNacimiento);
+            usuarioNuevo.setEmail(email);
+            usuarioNuevo.setTelefono(telefono);
+            usuarioNuevo.setDireccion(direccion);
+            usuarioNuevo.setEstado(true);
+
+            daoUsuario.GuardarUsuario(usuarioNuevo);
+        }
+
+    }
+}
